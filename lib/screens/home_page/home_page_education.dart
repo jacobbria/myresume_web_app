22c603bcd21bd1ed76a03354dd5cf71da942ee95
jacobbria <jacob.bria@sinclair.edu>
2024-11-14 21:20:51 -0500

    Last edits before vacation.

--- lib/screens/home_page/home_page_education.dart
+++ lib/screens/home_page/home_page_education.dart
@@ -8,7 +8,31 @@ class HomePageEducation extends StatelessWidget {
     return  Container(
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height,
-            color: Colors.blueGrey[400],
+            color: Theme.of(context).primaryColor,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 35),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        height:MediaQuery.of(context).size.height,
+                        color: Colors.blueGrey[200],
+                      ),
+                    ),
+                     Expanded(
+                      flex: 1,
+                       child: Container(
+                        height: MediaQuery.of(context).size.height,      
+                        color: Colors.blueGrey[100],
+                                           ),
+                     ),
+
+                  ],
+                ),
+              ),
+            
           );
   }
 }
